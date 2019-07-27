9c3ec20297872f5da9032ab46375ad8ee8042e14
mfazari <massimofazari@bluewin.ch>
2019-07-27 17:51:41 +0200

    Set up readline and CRUD

--- test.js
+++ test.js
@@ -8,24 +8,47 @@ var app = express();
 const { privateKey, publicKey } = await crypto2.createKeyPair();
 
 //Encryption
-function encryption(data) {
+async function encryption(data) {
     const encrypted = await crypto2.encrypt.rsa(data, publicKey);
     return encrypted;
 }
 
 //Decryption
-function decryption(secret){
+async function decryption(secret){
     const decrypted = await crypto2.decrypt.rsa(secret, privateKey);
-    return decrypted
+    return decrypted;
 
 }
 
+// Save in database
+app.post('/name/add', (req, res, next) => {
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+    var name = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+
+    dbase.collection("name").save(name, (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+
+        res.send('name added successfully');
+    });
 });
 
 
-app.listen(3000, function () {
+// Read from database
+app.get('/name', (req, res) => {
+    dbase.collection('name').find().toArray( (err, results) => {
+        res.send(results)
+    });
+});
+
+
+
+
+app.post(3000, function () {
     console.log('Example app listening on port 3000!');
 })
+
