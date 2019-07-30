83cd3b57fb21dc9e89872bb6d8f6825056da9bb1
mfazari <massimofazari@bluewin.ch>
2019-07-30 13:14:13 +0200

    functioning

--- test.js
+++ test.js
@@ -1,45 +1,22 @@
 // setting up our environment
 var express = require('express');
 const crypto2 = require('crypto2');
-const bodyParser= require('body-parser')
 var app = express();
 
-// Variables we take instead of database
-var encr;
-var decr;
 
-/*
-// What we need for our database connection
+// What we need for our database
 
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
-const bodyParser= require('body-parser')
- */
+const bodyParser= require('body-parser');
 
 // ... and some stuff to enable "post requests"
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-/*
-// Connect to our "sample" database and listening on port 3000
 
-MongoClient.connect('mongodb://localhost:27017/test', (err, db) => {
-    if (err) return console.log(err)
-
-    app.listen(3000, () => {
-        console.log('App listening on 3000')
-    });
-});
- */
-
-
-app.listen(3000, function () {
-    console.log('App listening on port 3000!');
-});
-
-
-// Create Keys (await deleted)
+// Create Keys
 const { privateKey, publicKey } = crypto2.createKeyPair();
 
 // Encryption
@@ -56,56 +33,59 @@ async function decryption(secret){
 }
 
 
-// Encrypt and save in database
-app.post('/data/add', (req, res, next) => {
+// Connect to our "sample" database and listening on port 3000
+MongoClient.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, (err, db) => {
+    if (err) return console.log(err)
 
-    let data = {
-        text : req.body.text
-    };
+    app.listen(3000, () => {
+        console.log('App listening on 3000')
+    });
 
-    // JSON to String
-    var string = JSON.stringify(data.text);
+    let dbase = db.db("crud");
 
-    // Get rid of "" or '' in String
-    var clean_string = string.replace(/["']/g, "");
+    // Encrypt and save in database
+    app.post('/data/add', (req, res, next) => {
+
+        let data = {
+            text : req.body.text
+        };
+
+        // JSON to String
+        let string = JSON.stringify(data.text);
+
+        // Get rid of "" or '' in String
+        let clean_string = string.replace(/["']/g, "");
 
 
-// Check for alphanumeric input
-    if(clean_string.match(/^([0-9]|[a-z])+([0-9a-z]+)$/i)) {
+        // Check for alphanumeric input
+        if(clean_string.match(/^([0-9]|[a-z])+([0-9a-z]+)$/i)) {
 
-        let encrypted_data = encryption(clean_string);
+            let encrypted_data = encryption(clean_string);
 
-        //temp
-        encr = encrypted_data;
+            dbase.collection('encrypted_data').insertOne(encrypted_data, (err, result) => {
+                if(err) {
+                    console.log(err);
+                }
 
-        /*
-        dbase.collection('encrypted_data').save(encrypted_data, (err, result) => {
-            if(err) {
-                console.log(err);
-            }
+                res.send('data added successfully');
+                console.log(encrypted_data); //test
+            });
 
-            res.send('data added successfully');
+        }
+        else{
+            res.send('ERROR: Only alphanumeric values are accepted!')
+        }
+    });
+
+
+    // Read and decrypt from database
+    app.get('/data', (req, res) => {
+
+        dbase.collection('encrypted_data').find().toArray( (err, results) => {
+            let decrypted_data = decryption(results);
+            res.send(decrypted_data);
+            console.log(decrypted_data); //test
         });
 
-         */
-    }
-    else{
-        res.send('ERROR: Only alphanumeric values are accepted!')
-    }
-});
-
-
-// Read and decrypt from database
-app.get('/data', (req, res) => {
-    /*
-    dbase.collection('encrypted_data').find().toArray( (err, results) => {
-        let decrypted_data = decryption(results);
-        res.send(decrypted_data);
     });
-     */
-    decr = decryption(encr);
-    res.send(decr);
-
-
 });
-
--- test2.js
+++ test2.js
@@ -1,91 +1,91 @@
 // setting up our environment
 var express = require('express');
-const crypto2 = require('crypto2');
 var app = express();
 
+// Our Encryption tool
+const NodeRSA = require('node-rsa');
 
 // What we need for our database
-
 const MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 const bodyParser= require('body-parser');
+var url = 'mongodb://localhost:27017/test';
+var dbase;
+
 
 // ... and some stuff to enable "post requests"
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
 // Create Keys
-const { privateKey, publicKey } = crypto2.createKeyPair();
-
-// Encryption
-async function encryption(data) {
-    const encrypted = await crypto2.encrypt.rsa(data, publicKey);
-    return encrypted;
-}
-
-// Decryption
-async function decryption(secret){
-    const decrypted = await crypto2.decrypt.rsa(secret, privateKey);
-    return decrypted;
-
-}
+const key = new NodeRSA({b: 512});
+let encrypted_data;
 
 
-// Connect to our "sample" database and listening on port 3000
-MongoClient.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, (err, db) => {
+// Connect to our database and listening on port 3000
+MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
     if (err) return console.log(err)
 
     app.listen(3000, () => {
-        console.log('App listening on 3000')
+        console.log('App listening on 3000');
+        console.log("Database created!");
+
+
     });
+    dbase = db.db("crud");
 
-    let dbase = db.db("crud");
+    // delete our old data
+    dbase.collection("data").deleteMany({ }, function(err, obj) {
+        if (err) throw err;
+        console.log(obj.result.n + " document(s) deleted");
+    });
+});
 
-    // Encrypt and save in database
-    app.post('/data/add', (req, res, next) => {
+// Encrypt and save in database
+app.post('/data/add', (req, res, next) => {
 
-        let data = {
-            text : req.body.text
+    let input_data = {
+        text : req.body.text
+    };
+
+    // JSON to String
+    let string = JSON.stringify(input_data.text);
+
+    // Get rid of "" or '' in String
+    let clean_string = string.replace(/["']/g, "");
+
+    // Check for alphanumeric input
+    if(clean_string.match(/^([0-9]|[a-z])+([0-9a-z]+)$/i)) {
+
+        let encrypted_string = key.encrypt(clean_string, 'base64');
+
+        encrypted_data = {
+            Word: encrypted_string
         };
 
-        // JSON to String
-        let string = JSON.stringify(data.text);
+        dbase.collection('data').insertOne(encrypted_data, (err, result) => {
+            if(err) {
+                console.log(err);
+            }
 
-        // Get rid of "" or '' in String
-        let clean_string = string.replace(/["']/g, "");
-
-
-        // Check for alphanumeric input
-        if(clean_string.match(/^([0-9]|[a-z])+([0-9a-z]+)$/i)) {
-
-            let encrypted_data = encryption(clean_string);
-
-            dbase.collection('encrypted_data').insertOne(encrypted_data, (err, result) => {
-                if(err) {
-                    console.log(err);
-                }
-
-                res.send('data added successfully');
-                console.log(encrypted_data); //test
-            });
-
-        }
-        else{
-            res.send('ERROR: Only alphanumeric values are accepted!')
-        }
-    });
-
-
-    // Read and decrypt from database
-    app.get('/data', (req, res) => {
-
-        dbase.collection('encrypted_data').find().toArray( (err, results) => {
-            let decrypted_data = decryption(results);
-            res.send(decrypted_data);
-            console.log(decrypted_data); //test
+            res.send('data added successfully');
         });
 
+    }
+    else{
+        res.send('ERROR: Only alphanumeric values are accepted!')
+    }
+});
+
+
+// Read and decrypt from database
+app.get('/data', (req, res) => {
+    dbase.collection('data').find(encrypted_data).toArray( (err, results) => {
+        let read_data = JSON.stringify(results[0].Word);
+        const decrypted = key.decrypt(read_data, 'utf8');
+        res.send(decrypted);
+        console.log(decrypted);
     });
+
 });
